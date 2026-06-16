681d5d385f59ca1835ed0c4124383f1e2a9e1ba6
DelioCoder <davideveriwhere@gmail.com>
2026-06-16 15:15:52 -0500

    Added: global variables for handling positions

--- js/config.js
+++ js/config.js
@@ -2,5 +2,12 @@ let imgRoute = "img";
 let maxHP = 100;
 let currentHP = 100;
 let bulletActive = false;
-let mainBoat, enemy, bullet;
+let mainBoat, enemy, enemyTop, enemyBottom, bullet;
 let rightKey, leftKey, upKey, downKey, spaceKey;
+
+const BATTLE_POSITIONS = {
+    mainBoatX: 1717 / 3 - 250,
+    mainBoatY: 916 / 2,
+    enemyX: 1717 / 2 + 450,
+    enemyY: 916 / 2
+};
