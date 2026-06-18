279f0053a9d2ac742aac16e43615e6a1530e9637
DelioCoder <davideveriwhere@gmail.com>
2026-06-18 01:36:45 -0500

    Updated: new enemies movement

--- js/enemy.js
+++ js/enemy.js
@@ -4,21 +4,26 @@ const ENEMY_ESCORT_OFFSET_X = 80;
 function createEnemy() {
     enemy = game.add.sprite(BATTLE_POSITIONS.enemyX, BATTLE_POSITIONS.enemyY, 'enemyBoat');
     enemy.anchor.setTo(0.5);
-    enemy.frame = 1;
-    enemy.scale.x = 1;
+    enemy.scale.x = -1;
+    enemy.width = 300; enemy.height = 250;
+    enemy.animations.add('move', [0, 1, 2, 3], 6, true);
+    enemy.animations.play('move');
     game.physics.arcade.enable(enemy);
 
-    // Botes refuerzo
     enemyTop = game.add.sprite(BATTLE_POSITIONS.enemyX + ENEMY_ESCORT_OFFSET_X, BATTLE_POSITIONS.enemyY - ENEMY_ESCORT_OFFSET_Y, 'enemyBoat');
     enemyTop.anchor.setTo(0.5);
-    enemyTop.frame = 1;
-    enemyTop.scale.x = 1;
+    enemyTop.scale.x = -1;
+    enemyTop.width = 300; enemyTop.height = 250;
+    enemyTop.animations.add('move', [0, 1, 2, 3], 6, true);
+    enemyTop.animations.play('move');
     game.physics.arcade.enable(enemyTop);
 
     enemyBottom = game.add.sprite(BATTLE_POSITIONS.enemyX + ENEMY_ESCORT_OFFSET_X, BATTLE_POSITIONS.enemyY + ENEMY_ESCORT_OFFSET_Y, 'enemyBoat');
     enemyBottom.anchor.setTo(0.5);
-    enemyBottom.frame = 1;
-    enemyBottom.scale.x = 1;
+    enemyBottom.scale.x = -1;
+    enemyBottom.width = 300; enemyBottom.height = 250;
+    enemyBottom.animations.add('move', [0, 1, 2, 3], 6, true);
+    enemyBottom.animations.play('move');
     game.physics.arcade.enable(enemyBottom);
 }
 
